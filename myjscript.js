dc06f60a6b5c34e2bf087f04d735c4ba70d93006
Renlly Bacolina <bacolinarenlly@gmail.com>
2024-11-12 00:27:07 +0800

    first commit

---
+++ myjscript.js
@@ -0,0 +1,27 @@
+(function() {
+    emailjs.init("JG95twLTVTAz-JU36");
+  })();
+  
+  function sendMail() {
+    const parms = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    emailjs.send("service_7izh5mg", "template_uk0fuvh", parms)
+      .then(() => {
+        alert("Email Sent!");
+        document.getElementById("contact-form").reset();
+      })
+      .catch(error => {
+        console.error("Failed to send email:", error);
+        document.getElementById("status-message").textContent = "Failed to send message. Please try again.";
+      });
+  }
+  
+  document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    sendMail();
+  });
+  
