38d2930962ba25871a0a435437ca2fb1e3c2b964
Renlly Bacolina <bacolinarenlly@gmail.com>
2024-11-12 10:26:15 +0800

    test commmit

--- myjscript.js
+++ myjscript.js
@@ -25,3 +25,10 @@
     sendMail();
   });
   
+
+  const navbarToggle = document.querySelector('.navbar-toggle');
+  const navbarLinks = document.querySelector('.navbar-links');
+  
+  navbarToggle.addEventListener('click', () => {
+      navbarLinks.classList.toggle('active'); // Toggles the 'active' class to show/hide navbar links
+  });
